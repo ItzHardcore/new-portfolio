dc21e57d5fef5b39a22db82126b6eaea121f5877
Bruno Silva <bruno.vp.1999@gmail.com>
2024-05-21 10:00:31 +0100

    tech icons

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
+import { Col, Row, Tooltip, OverlayTrigger } from "react-bootstrap";
 import {
   DiJavascript1,
   DiReact,
@@ -11,59 +10,169 @@ import {
   DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
+  SiMysql,
+  SiGooglecloud,
+  SiPhp,
+  SiExpress,
   SiFirebase,
+  SiRedis,
   SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
+  SiRedux,
+  SiSolidity,
+  SiVercel,
+  SiVuedotjs,
+  SiStyledcomponents,
+  SiBootstrap,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip1">JavaScript</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiJavascript1 />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip2">React</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiReact />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip3">Node.js</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiNodejs />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip4">MongoDB</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiMongodb />
+        </Col>
+      </OverlayTrigger>
+
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip11">Express.js</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiExpress />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip6">Git</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiGit />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip7">Java</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiJava />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip8">MySQL</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiMysql />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip9">Google Cloud</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiGooglecloud />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip10">PHP</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiPhp />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip5">Python</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <DiPython />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip14">Vue.js</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiVuedotjs />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip15">PostgreSQL</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiPostgresql />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip16">Redux</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiRedux />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip17">Styled Components</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiStyledcomponents />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={<Tooltip id="tooltip17">Bootstrap</Tooltip>}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiBootstrap />
+        </Col>
+      </OverlayTrigger>
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,98 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Tooltip, OverlayTrigger } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
   SiSlack,
   SiVercel,
   SiMacos,
+  SiWindows,
+  SiJira,
+  SiBitbucket,
+  SiFirebase,
+  SiNpm,
 } from "react-icons/si";
 
 function Toolstack() {
+  const tooltip = (id, text) => (
+    <Tooltip id={id}>
+      <strong>{text}</strong>
+    </Tooltip>
+  );
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip1", "Windows")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiWindows />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip2", "Visual Studio Code")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiVisualstudiocode />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip3", "Postman")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiPostman />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip4", "Jira")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiJira />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip5", "Vercel")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiVercel />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip5", "Firebase")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiFirebase />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip5", "Bitbucket")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiBitbucket />
+        </Col>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="bottom"
+        overlay={tooltip("tooltip5", "NPM")}
+      >
+        <Col xs={4} md={2} lg={3} className="tech-icons">
+          <SiNpm />
+        </Col>
+      </OverlayTrigger>
     </Row>
   );
 }
