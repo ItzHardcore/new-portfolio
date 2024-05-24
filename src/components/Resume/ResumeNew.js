ffb61535f16d6f7731dbe8c1f05fcf12481c3798
Bruno Silva <bruno.vp.1999@gmail.com>
2024-05-24 11:29:55 +0100

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,52 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/cv.pdf";
+import pdf from "../../Assets/../Assets/CV_Bruno_Silva.pdf";
+import pdfjpeg from "../../Assets/../Assets/pdf.jpg";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+  function onDocumentLoadSuccess(numPages) {
+    setNumPages(numPages);
+  }
 
   return (
     <div>
       <Container fluid className="resume-section">
-        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+        <div className="py-3">
+          <object
+            className="resume"
+            data={`${pdf}#toolbar=1&navpanes=0`}
+            type="application/pdf"
+            width="100%"
+            height="100%" >
+            <div className="d-inline-grid" style={{ justifyItems: "center" }}>
+
+              <a className="mb-3 p-3" href={pdf}><Button
+                variant="primary"
+              >
+                <AiOutlineDownload />
+                &nbsp;Download CV
+              </Button></a>
+              <img src={pdfjpeg} alt="pdf" className="w-50"></img>
+            </div>
+          </object>
+        </div>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row> */}
-        <p>On Maintenance</p>
+
+        </Row>
       </Container>
     </div>
   );
